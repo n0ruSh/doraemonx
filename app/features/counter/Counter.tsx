309e8f79605869d068bf1d66671bbdaad75d3191
n0ruSh <zhengjianhua@xiaojuchefu.com>
2020-08-15 10:53:26 +0800

    feat: update readme and package.json

--- app/features/counter/Counter.tsx
+++ app/features/counter/Counter.tsx
@@ -53,7 +53,7 @@ export default function Counter() {
           data-tclass="btn"
           type="button"
         >
-          odd
+          odd1111
         </button>
         <button
           className={styles.btn}
